5fb0e2bad8f6b36cf97a42a37a688f914824095e
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-03-13 11:06:47 +0100

    creating project

--- src/main/java/pl/zajavka/Main.java
+++ src/main/java/pl/zajavka/Main.java
@@ -1,7 +1,38 @@
 package pl.zajavka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Karol Janowski");
+        LOGGER.trace("Some messange {}", 123);
+        LOGGER.debug("Some messange {}", 123);
+        LOGGER.info("Some messange {}", 123);
+        LOGGER.warn("Some messange {}", 123);
+        LOGGER.error("Some messange {}", 123);
+
+        try {
+            method1();
+        } catch (Exception ex) {
+            LOGGER.error("Exception was thrown", ex);
+        }
+    }
+
+    private static void method1() {
+        method2();
+    }
+    private static void method2() {
+        method3();
+    }
+    private static void method3() {
+        method4();
+    }
+    private static void method4() {
+        method5();
+    }
+    private static void method5() {
+        throw new RuntimeException("Throwing some exception!");
     }
 }
