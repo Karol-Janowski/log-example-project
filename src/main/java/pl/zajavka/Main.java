57fb9a59cd8ea15150f07631df90e8b3b435225e
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-03-18 19:51:04 +0100

    mini bank project

--- src/main/java/pl/zajavka/Main.java
+++ src/main/java/pl/zajavka/Main.java
@@ -2,39 +2,30 @@ package pl.zajavka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.zajavka.miniProject.BankAccount;
+import pl.zajavka.miniProject.BankService;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        LOGGER.trace("Some messange {}", 123);
-        LOGGER.debug("Some messange {}", 123);
-        LOGGER.info("Some messange {}", 123);
-        LOGGER.warn("Some messange {}", 123);
-        LOGGER.error("Some messange {}", 123);
 
-        LoggerLoop.log();
+        BankService.createNewAccount(1L, "Karol Janowski", BigDecimal.valueOf(1000));
+        BankService.createNewAccount(2L, "Marcel Kawalec", BigDecimal.valueOf(5000));
+        BankService.createNewAccount(3L, "Milosz Grabowski", BigDecimal.valueOf(2500));
+        BankService.createNewAccount(4L, "Janusz Transportu", BigDecimal.valueOf(500));
 
-//        try {
-//            method1();
-//        } catch (Exception ex) {
-//            LOGGER.error("Exception was thrown", ex);
-//        }
-    }
+//        BankService.depositMoney(4L, BigDecimal.valueOf(1000));
+//        BankService.withdrawMoney(1L, BigDecimal.valueOf(500));
 
-    private static void method1() {
-        method2();
-    }
-    private static void method2() {
-        method3();
-    }
-    private static void method3() {
-        method4();
-    }
-    private static void method4() {
-        method5();
-    }
-    private static void method5() {
-        throw new RuntimeException("Throwing some exception!");
+        BankService.transferMoney(1L, 5L, BigDecimal.valueOf(500));
+
+       for (BankAccount bankAccount : BankService.getBankAccountsList()) {
+           System.out.println(bankAccount);
+       }
     }
 }
